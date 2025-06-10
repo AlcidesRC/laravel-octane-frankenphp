f4386a3150090b60ef9b8f3e31702dc8020d133c
Alcides Ramos <alcides.ramos@leadtech.com>
2025-06-10 14:26:52 +0200

    fix: created </post> endpoint for AB stress test case

--- src/routes/web.php
+++ src/routes/web.php
@@ -5,3 +5,10 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::post('/post', function (Illuminate\Http\Request $request) {
+    return response()->json([
+        'param1' => $request->get('param1'),
+        'param2' => $request->get('param2'),
+    ]);
+});
